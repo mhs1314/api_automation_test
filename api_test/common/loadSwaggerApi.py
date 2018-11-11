4f82bcc9baf48d9205c5d1ebc95cdbf5870b8327
mhs <471002385@qq.com>
2018-11-12 01:26:28 +0800

    swagger-api 导入修改--1

--- api_test/common/loadSwaggerApi.py
+++ api_test/common/loadSwaggerApi.py
@@ -85,7 +85,7 @@ def swagger_api(url, project, user):
                 for j in data["responses"]:
                     if "schema" in data["responses"]["200"]:
                         ref = data["responses"]["200"]["schema"]["$ref"]
-                        dto = ref.split("／")
+                        dto = ref.split("\／")
                         logging.error(dto)
                         for key, value in params[dto]["properties"].items():
                             if "description" in j:
