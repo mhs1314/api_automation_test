2c0a78f4ca13c298d2984cc008f41060cd601c78
mhs <471002385@qq.com>
2018-11-13 00:02:37 +0800

    swagger-api 导入修改--1

--- api_test/common/loadSwaggerApi.py
+++ api_test/common/loadSwaggerApi.py
@@ -95,6 +95,7 @@ def swagger_api(url, project, user):
                                 ref1 = value["items"]["$ref"]
                                 dto1 = ref1.split("/")[2]
                                 for key1, value1 in params[dto1]["properties"].items():
+                                    logging.error(key1)
                                     if "description" in value1 and "type" in value1:
                                         response.append({"name": key + "." +key1, "value": value1["description"], "_type": value1["type"],"required": True, "description": value1["description"]})
                                     else:
